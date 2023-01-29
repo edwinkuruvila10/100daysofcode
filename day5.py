24be8174d35f7d9e4b3f3d48451305e886caea46
edwinkuruvila10 <edwinkuruvila089@gmail.com>
2023-01-29 13:44:04 +0530

    Create an calculator

---
+++ day5.py
@@ -0,0 +1,21 @@
+print(5+6)
+print(8-3)
+print(55*3)
+print(9/2)
+
+a=15
+b=7
+ans1 = a+b
+print("Addition of",a,"and",b,"is",ans1)
+ans2 = a*b
+print("Multiplication of",a,"and",b,"is",ans2)  
+ans3 = a/b
+print("division of",a,"and",b,"is",ans3)
+
+#creating a calculator
+a=int(input("enter first no."))
+b=int(input("enter second no."))
+print("sum is ",a+b)
+print("min is ",a-b)
+print("mul is ",a*b)
+print("div is ",a/b)
