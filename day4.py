3d22298ba16e28f565a7f8018b4a5a4d30d9fc4f
edwinkuruvila10 <edwinkuruvila089@gmail.com>
2023-01-29 13:18:01 +0530

    variables and data types

---
+++ day4.py
@@ -0,0 +1,24 @@
+a=9
+b=True  
+c="harry"
+d=None
+print(a)
+print(b)
+a1=10
+print(a+a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+
+list1 = [8, 2.3, [-4,5], ["Apple", "banana"]]
+print(list1)
+
+tuple1 = (("lion", "sparrow"),("pig","tiger"))
+print(tuple1)
+
+dict1 = {"name":"edwin", "age":20, "can he win":True}
+print(dict1)
+
+
+
+
