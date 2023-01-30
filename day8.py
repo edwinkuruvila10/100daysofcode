727beea71c96409e4822a5922b592eb1ed2408e1
edwinkuruvila10 <edwinkuruvila089@gmail.com>
2023-01-30 12:11:42 +0530

    String

---
+++ day8.py
@@ -0,0 +1,18 @@
+name="harry"
+friend="edwin"
+apple='''He said,
+Hi Mary
+hey i am good
+"i want to eat an apple'''
+
+print("hello," + name)
+print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+print("lets use a for loop\n")
+for character in name:
+    print(character)
