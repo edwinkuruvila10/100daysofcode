08ef21be1500ee48657a86cb892874a1b23f46ca
edwinkuruvila10 <edwinkuruvila089@gmail.com>
2023-02-06 22:24:16 +0530

    String Slicing

---
+++ day9.py
@@ -0,0 +1,20 @@
+import smtpd
+
+
+fruit="mango"
+len1=len(fruit)
+print("Mango is a",len1,"letter word.")
+
+fruit="mango"
+mangolen=len(fruit)
+print(mangolen)
+print(fruit[0:4])
+print(fruit[1:4])
+print(fruit[:5]) 
+
+print(fruit[-1:len(fruit) - 3])
+print(fruit[-3:-1])
+
+#quiz
+nm="Harry"
+print(nm[-4:-2])
