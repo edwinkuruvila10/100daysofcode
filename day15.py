70c748263d43b314cc10c0cbe68c85e31b5355a9
edwinkuruvila10 <edwinkuruvila089@gmail.com>
2023-02-12 21:12:02 +0530

    While loop

---
+++ day15.py
@@ -0,0 +1,17 @@
+i = 0
+while(i<=3):
+    print(i)
+    i = i + 1
+print("Done with the loop")
+
+
+i = int(input("Enter the number: "))
+while(i<=38):
+    i = int(input("Enter the number: "))
+    print(i)
+print("Done with the loop")
+
+count = 5
+while (count > 0):
+    print(count)
+    count = count - 1
