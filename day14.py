ca688979d270b39b25bc67522a8523c2a194ce28
edwinkuruvila10 <edwinkuruvila089@gmail.com>
2023-02-12 16:21:28 +0530

    for loop

---
+++ day14.py
@@ -0,0 +1,25 @@
+#for
+name = "Edwin"
+for i in name:
+    print(i)
+    if(i =="d"):
+        print("This is something special")
+
+colors = ["Red", "Green", "Blue", "yellow"]
+for color in colors:
+  print(color)
+  for i in color:
+     print(i) 
+
+#range
+for k in range(5):
+    print(k + 1)
+
+for k in range(1, 20001):
+    print(k)
+
+for k in range(1, 12, 3):
+    print(k)
+
+
+
