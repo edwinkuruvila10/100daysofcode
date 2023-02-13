3ed54f581b830690a164e349058f7d40bd95f2cb
edwinkuruvila10 <edwinkuruvila089@gmail.com>
2023-02-13 16:08:26 +0530

    Break and continue

---
+++ day16.py
@@ -0,0 +1,23 @@
+# #Break statement
+# for i in range(12):
+#     if(i == 10):
+#         break
+#     print("5 x ", i+1, "=", 5 * (i+1))
+
+#     print(" i what to leave the channel  ")
+
+# #continue statment
+# for i in range(12):
+#     if(i == 10):
+#       print(" i what to leave the channel  ")
+#       continue 
+#     print("5 x ", i, "=", 5 * i)
+
+i=0
+while True:
+    print(i)
+    i = i + 1
+    if(i%100 == 0):
+        break
+
+
