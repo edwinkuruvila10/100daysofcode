3903a7a04fdb8e1f7b3a42878f275266c78591bd
edwinkuruvila10 <edwinkuruvila089@gmail.com>
2023-02-21 23:58:52 +0530

    lists Methods

---
+++ day20.py
@@ -0,0 +1,22 @@
+#list sort()
+l = [11, 45, 1, 2, 4, 6]
+print(l)
+# l.append(7)
+
+# l.sort(reverse=True)
+# l.reverse()
+
+# print(l.index(1))
+
+# print(l.count(1))
+
+# m = l.copy()
+# m[0] = 0
+
+# l.insert(1, 899)
+
+m = [900, 1000, 1100]
+k = l + m
+print(k)
+# l.extend(m)
+print(l)
