d45ae29bb02434c6bed89f7d2329284c1cdb73f1
edwinkuruvila10 <edwinkuruvila089@gmail.com>
2023-02-22 20:46:08 +0530

    Operation of Tuples

---
+++ day22.py
@@ -0,0 +1,18 @@
+# countries = ("spain", "india", "south", "south-east", "south-west")
+# temp = list(countries)
+# temp.append("kerala")
+# temp.pop(3)
+# temp[2] = "finland"
+# countries = tuple(temp)
+# print(countries)
+
+#Tuple methods for count
+# tuple1 = (1, 2, 3, 3, 5, 3, 7, 3, 9, 10)
+# res = tuple1.count(3)
+# print("count of 3 in tuple1 is:", res)
+
+#index methods
+tuple1 = (1, 2, 3, 3, 5, 3, 7, 3, 9, 10)
+# res = tuple1.index(3)
+res = len(tuple1)
+print("count of 3 in tuple1 is:", res)
