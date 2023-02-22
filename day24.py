ab818bdac0885e1887dc5d482c1ed1f4d65fd481
edwinkuruvila10 <edwinkuruvila089@gmail.com>
2023-02-22 22:19:29 +0530

    Exercise 3

---
+++ day24.py
@@ -0,0 +1,17 @@
+# Exercise3:-
+KBC="Welcome to KBC, your question for One Million is"
+enter="Press Enter Key to Proceed: "
+enter1="Type in your answer and press enter: "
+Q1="Q1: who is not a member of shivam house"
+list=("Pooja","Mauli","Kajal","Stuti")
+print(KBC)
+print(input(enter))
+print(Q1)
+print(list)
+
+typ=input("Type in your answer and press enter: ")
+if typ == "Pooja":
+ print("CONGRATULATION YOU WON ON MILLION")
+else:
+  print("SORRY, ITS WRONG ANSWER")
+
