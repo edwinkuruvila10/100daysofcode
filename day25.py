3068f4d75cd993701cac98c3f4939032268a412f
edwinkuruvila10 <edwinkuruvila089@gmail.com>
2023-02-23 15:48:11 +0530

    f-Strings

---
+++ day25.py
@@ -0,0 +1,9 @@
+Letter = "Hey my name is {1} and I am from {0}"
+country = "india"
+name = "edwin"
+print(Letter.format(country, name))
+print(f"Hey my name is{name} and I am from {country}")
+
+price= 49.000
+txt = f"for only {price:.2f}dollers"
+print(txt)
