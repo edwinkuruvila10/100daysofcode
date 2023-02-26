d016c8f0fd23b8fd2b7e875e62b327d89d283d46
edwinkuruvila10 <edwinkuruvila089@gmail.com>
2023-02-26 15:02:08 +0530

    Exercise 4 : Secret code language

---
+++ day37.py
@@ -0,0 +1,17 @@
+a = input ("Enter the message : ")
+import random
+H = random.choice(["dhj" , "jkh" ,"rty"])
+B = random.choice(["abc","tyu","iop"])
+print(B+a[1:Len(a)]+a[0:1]+H)
+
+print("for decode add two No:")
+import random
+C = random.choice([3,5,7,9])
+D = random.choice([1,2,4,5])
+print(C)
+print(D)
+F = int(input ("enter the no :"))
+if  ( F==C+D) :
+    print(a)
+else :
+    print("you are wrong")
