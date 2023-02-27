b8ddd2d9c5146d29d05ec17f47fff46dab37eef0
edwinkuruvila10 <edwinkuruvila089@gmail.com>
2023-02-27 23:36:13 +0530

    Enumerate Function

---
+++ day39.py
@@ -0,0 +1,15 @@
+marks = [122, 56, 32, 98, 12, 45, 1, 4]
+
+# index = 0
+# for mark in marks:
+#     print(mark)
+#     if(index == 3):
+#         print("edwin, smart")
+#     index +=1      
+
+
+#Using enumerate
+for index, mark in enumerate (marks):
+    print(mark)
+    if(index == 3):
+        print("edwin, smart")   
